eaed1ce5da21dc25295dc231e4c8ff4d28f7f4df
kiDra94 <drazen.p@live.com>
2024-05-16 18:23:32 +0200

    zadatakDEF

--- ServiceApplicationsDevelopment/Live/XML/zadatak.py
+++ ServiceApplicationsDevelopment/Live/XML/zadatak.py
@@ -1,12 +1,15 @@
 N = 10
 
-mat = [[1 if i == 0 or i == N-1 or j == 0 or j == N-1 else 0 for j in range(N)] for i in range(N)]
+def obrada(N):
+    return [[1 if i == 0 or i == N-1 or j == 0 or j == N-1 else 0 for j in range(N)] for i in range(N)]
+
 def ispis(mat):
     for r in mat:
         print(r)
 
-ispis(mat)
-print("---------------------------")
+ispis(obrada(N))
+
+print("-----------------------------------")
 
 mat1 = []
 for i in range(N):
